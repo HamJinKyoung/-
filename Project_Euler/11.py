0fb748938a8c0c06a80322d0b08d2716c7e3b7a1
HamJinkyoung <wlsrud3803@gmail.com>
2020-05-06 23:18:49 +0900

    왜틀렸을까?

--- Project_Euler/11.py
+++ Project_Euler/11.py
@@ -81,7 +81,7 @@ def diagonal(matrix): # 대각선(\)방향
 
     # 왼쪽부분
     for i in range(1,17):
-        for j in range(15):
+        for j in range(16):
             mul = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3]
             if max < mul:
                 max = mul
